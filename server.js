fd1da61176852d6a548daaf748825e9c8ca48b95
black8cherry <art8evd@gmail.com>
2019-12-21 16:19:01 +0300

    desigen

--- server.js
+++ server.js
@@ -1,97 +1,100 @@
-const express        = require('express');
-const bodyParser     = require('body-parser');
-const app            = express();
-var MongoClient      = require('mongodb').MongoClient;
-var http             = require('http').Server(app);
-var database_;
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+var MongoClient = require('mongodb').MongoClient;
+var http = require('http').Server(app);
+var books;
+var user;
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
-  if (req == null){
-    database_.collection('books').find({"name" : req.body.name}).toArray((err, items) => {
-      res.render('../views/pages/main.ejs', {
-        books: items
-      });
-    });
-  } else {
-    database_.collection('books').find({}).toArray((err, items) => {
-      res.render('../views/pages/main.ejs', {
-        books: items
-      });
-    });
-  }
-});
-//var myCursor = db.inventory.find();
-//var myDoc = myCursor.hasNext() ? myCursor.next() : null;
+MongoClient.connect('mongodb+srv://artem:h0Sw19XgFppbsf3X@test-cmnmp.mongodb.net/test?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  (err, database) => {
+    if (err) {
+      return console.log(err);
+    }
 
-app.post('/', function(req, res){
-  var books = database_.collection('books');
-  var user = database_.collection('user');
-  if (
-    books.findOne({"id" : req.body.bookId}).count >= 1
-    // && 
-  ) {
-    
-    books.findOneAndUpdate({"id" : req.body.bookId}, {$inc : {"count": -1}},(err, res) => {
-      err ? console.log('err books') : console.log('book minus')
-    });
-    user.insertOne(books.findOne({"id" : req.body.bookId}), (err, res) => {
-      err ? console.log('err user') : console.log('user plus')
+    books = database.db('database').collection('books');
+    user = database.db('database').collection('user', (err, res) => {
+      res.estimatedDocumentCount((err, count) => {
+        if (count == 0) {
+          database.db('database').createCollection('user');
+        }
+      })
     });
 
-    var d = new Date();
-    var date = new Date().setFullYear(d.getYear(), d.getMonth() + 1, d.getDate() + 20).toDateString();
-    console.log(date);
-    user.aggregate(user.findOne({"id" : req.body.bookId}), { $addFields: {"date" : date}})
+    app.listen(3000, function () {
+      console.log(`Работаем: http://localhost:3000`);
+    });
+  });
 
-    res.redirect('/');
-  } else {
-    console.log('book count less then 1 or user already had');
-    res.redirect('/');
-  }
-})
-
-app.get('/user', function(req, res){
-    res.render('../views/pages/user.ejs');
+app.get('/', (req, res) => {
+  books.find({}).toArray((err, items) => {
+    res.render('../views/pages/main.ejs', {
+      books: items
+    });
+  });
 });
 
-app.get('/addBook', function(req, res){
+app.post('/', (req, res) => {
+  user.estimatedDocumentCount((err, count) => {
+    if (count) {
+      res.send({ 'error': 'return book first!' });
+    } else {
+      books.findOneAndUpdate({ 'id': req.body.book_id }, { $inc: { 'count': -1 } }, (err, book) => {
+        if (book.value.count == 0) {
+          books.findOneAndUpdate({ 'id': book.value.id }, { $set: { 'count': 0 } });
+          res.redirect('/');
+        } else {
+          book.value.count = 1;
+
+          var date = new Date();
+          date.setFullYear(date.getFullYear(), date.getMonth() + 1, date.getDate() + 20);
+          date = date.getDate() + '.' + date.getMonth() + '.' + date.getFullYear();
+
+          user.insertOne({ ...book.value, 'date': date });
+          res.redirect('/');
+        }
+      });
+    }
+  });
+});
+
+app.get('/user', (req, res) => {
+  user.find({}).toArray((err, items) => {
+    res.render('../views/pages/user.ejs', {
+      books: items
+    });
+  });
+});
+
+app.post('/user', (req, res) => {
+  books.findOneAndUpdate({ 'id' : req.body.book_id }, { $inc: { 'count' : 1 }});
+  user.deleteOne({ 'id' : req.body.book_id});
+  res.redirect('/user');
+});
+
+app.get('/addBook', (req, res) => {
   res.render('../views/pages/addBook.ejs');
 });
 
 app.post('/addBook', (req, res) => {
   console.log(req.body.id);
-  
-  var book = { id: req.body.id, name: req.body.name, count: req.body.count};
 
-  database_.collection('books').insertOne(book, (err, result) => {
-    if (err) { 
-      res.send({ 'error': 'An error has occurred' }); 
+  var book = { 'id': req.body.id, 'name': req.body.name, count: Number(req.body.count) };
+
+  books.insertOne(book, err => {
+    if (err) {
+      res.send({ 'error': 'An error has occurred' });
     } else {
       res.redirect('/');
     }
   });
-});
-
-MongoClient.connect('mongodb+srv://artem:h0Sw19XgFppbsf3X@test-cmnmp.mongodb.net/test?retryWrites=true&w=majority', 
-                    {useNewUrlParser: true,
-                    useUnifiedTopology: true}, 
-                    function(err, database
-){
-    if(err){
-         return console.log(err);
-    }
-
-    database_ = database.db('database');
-    if(database_.collection('user'))
-      database_.collection('user').drop();
-    database_.collection('user');
-
-    app.listen(3000, function() {
-        console.log(`Работаем: http://localhost:3000`);
-    });
 });
